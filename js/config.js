ecef433b0219d0131c9cc702b90f42432f7d8df7
EC2 Default User <ec2-user@ip-172-31-36-114.ap-northeast-2.compute.internal>
2025-02-27 06:56:36 +0000

    v123123132

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_P3HcAAzbs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2haffskog750jr339smftvh1el', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '10ubo6eei8vgcpidg83lid9ise', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
